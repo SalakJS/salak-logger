98cbcbcf7f9ae97d25ea0e82e1743de6b0b73015
wengeek <wenwei897684475@gmail.com>
2018-07-09 21:10:44 +0800

    init

---
+++ README.md
@@ -0,0 +1,78 @@
+# salak-logger
+
+[![NPM version][npm-image]][npm-url]
+[![build status][travis-image]][travis-url]
+[![David deps][david-image]][david-url]
+[![NPM download][download-image]][download-url]
+
+[npm-image]: https://img.shields.io/npm/v/salak-logger.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/salak-logger
+[travis-image]: https://img.shields.io/travis/SalakJS/salak-logger.svg?style=flat-square
+[travis-url]: https://travis-ci.org/SalakJS/salak-logger
+[david-image]: https://img.shields.io/david/SalakJS/salak-logger.svg?style=flat-square
+[david-url]: https://david-dm.org/SalakJS/salak-logger
+[download-image]: https://img.shields.io/npm/dm/salak-logger.svg?style=flat-square
+[download-url]: https://npmjs.org/package/salak-logger
+
+Salak logger, base on [winston](https://github.com/winstonjs/winston).
+
+## Install
+
+```bash
+npm install --save salak-logger
+```
+
+## Usage
+
+```javascript
+const Koa = require('koa')
+const logger = require('salak-logger')
+
+const app = new Koa()
+app.baseDir = __dirname
+app.logger = logger({
+  fileType: 'dateFile',
+  formatType: 'log4js',
+  defaultLevel: 'debug'
+}, app)
+
+app.logger.info('log')
+```
+
+## Options
+
+- `root` **{String}** directory for storing logs, default `path.join(app.baseDir, 'logs')`
+- `injectConsole` **{Boolean}** inject console transport for logger，default `app.env === 'developmemt'`
+- `formatType` **{String}** type for logger format, log4js or json, default `log4js`
+- `fileType` **{String}** file type for storing log, file or dateFile, default `file`
+- `category` **{Object}** optional, default `undefined`
+- `categories` **{Object}** optional, be used when category was unset
+- `capture` **{Object}** options to pass to the http request log
+- `defaultLevel` **{String}** log output level, default `app.env === 'production' ? 'info' : 'debug'`
+- `transports` **{Object}** transports for category
+
+## options for transports
+
+type: Map
+
+- `key` **{String}** transport name
+- `value` **{Object}** transport options
+- `value.type` **{String}** can be set to 'console', 'file', 'http', 'stream', 'dateFile', refer [transport options](https://github.com/winstonjs/winston/blob/master/docs/transports.md), [dateFile options](https://github.com/winstonjs/winston-daily-rotate-file#options)
+
+## options for category
+
+- `transports` **{Array<String|Transport>}** transport referred to transports which defined
+- `level` **{String}** log level， default `${options.defaultLevel}`
+
+## options for categories
+
+type: Map
+
+- `key` **{String}** category name
+- `value` **{Object}** category options
+- `value.transports` **{Array<String|Transport>}** transport referred to transports which defined
+- `value.level` **{String}** log level， default `${options.defaultLevel}`
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,155 @@
+const assert = require('assert')
+const winston = require('winston')
+const DailyRotateFile = require('winston-daily-rotate-file')
+const fse = require('fs-extra')
+const path = require('path')
+const format = require('./lib/format')
+const httpLog = require('./lib/http')
+
+const transportsType = {
+  console: winston.transports.Console,
+  file: winston.transports.File,
+  http: winston.transports.Http,
+  stream: winston.transports.Stream,
+  dateFile: DailyRotateFile
+}
+
+module.exports = (options = {}, app) => {
+  const { baseDir, env } = app
+  const {
+    root = path.join(baseDir, 'logs'),
+    injectConsole = env === 'development',
+    formatType = 'log4js',
+    fileType = 'file',
+    capture = {
+      enable: true,
+      category: 'http',
+      level: 'auto'
+    },
+    defaultLevel = env === 'production' ? 'info' : 'debug'
+  } = options
+  const isSingleCategory = !!options.category
+  const categories = isSingleCategory ? {
+    default: options.category
+  } : Object.assign({}, {
+    default: {
+      transports: [
+        'default',
+        'error'
+      ]
+    },
+    app: {
+      transports: [
+        'app',
+        'error'
+      ]
+    },
+    http: {
+      transports: [
+        'http'
+      ]
+    }
+  }, options.categories)
+  const createFileTransport = ({
+    type = fileType,
+    level,
+    filename
+  }) => {
+    return {
+      type: transportsType[type] ? type : 'file',
+      level,
+      filename: isSingleCategory ? `${filename}.log` : `${filename}/${filename}.log`
+    }
+  }
+  const transports = Object.assign({}, {
+    console: {
+      type: 'console'
+    },
+    default: createFileTransport({ filename: 'default' }),
+    app: !isSingleCategory && createFileTransport({ filename: 'app' }),
+    http: !isSingleCategory && createFileTransport({ filename: 'access' }),
+    error: createFileTransport({ filename: 'error', level: 'error' })
+  }, options.transports)
+
+  fse.ensureDirSync(root)
+
+  const createTransportInstance = (key, options) => {
+    const type = options && options.type
+    assert(type && transportsType[type], `unknown type ${type}.`)
+
+    if (key === 'console') {
+      options = Object.assign({}, {
+        format: format.consoleFormat()
+      }, options)
+    }
+
+    if (type === 'file' || type === 'dateFile') {
+      assert(options.filename, `transport ${key} must provide filename option.`)
+      options.filename = path.join(root, options.filename)
+      fse.ensureDirSync(path.dirname(options.filename))
+    }
+
+    return new transportsType[type](options)
+  }
+
+  const transportInstances = {}
+  for (let key in transports) {
+    if (transports[key]) {
+      transportInstances[key] = createTransportInstance(key, transports[key])
+    }
+  }
+
+  if (injectConsole) {
+    for (let key in categories) {
+      assert(categories[key].transports, `${key} must provide transport option`)
+      if (categories[key].transports.indexOf('console') === -1) {
+        categories[key].transports.push('console')
+      }
+    }
+  }
+
+  const createLogger = (label, {
+    transports = ['default'],
+    level = defaultLevel
+  }) => {
+    return winston.createLogger({
+      level,
+      format: format.combine(label, formatType),
+      exitOnError: false,
+      transports: transports.map((item) => {
+        if (typeof item === 'string') {
+          return transportInstances[item]
+        }
+
+        return item
+      }).filter((item) => !!item)
+    })
+  }
+
+  const target = {}
+  for (let key in categories) {
+    target[key] = createLogger(key, categories[key])
+  }
+
+  const levels = ['silly', 'debug', 'verbose', 'info', 'warn', 'error']
+  levels.forEach((method) => {
+    const logger = target['default']
+    target[method] = logger[method].bind(logger)
+  })
+
+  const loggerObj = new Proxy(target, {
+    get (target, key, receiver) {
+      if (!target[key]) {
+        target[key] = createLogger(key, categories['default'])
+      }
+
+      return Reflect.get(target, key, receiver)
+    }
+  })
+
+  if (capture.enable !== false) {
+    app.use(httpLog(capture, app))
+  }
+
+  return loggerObj
+}
